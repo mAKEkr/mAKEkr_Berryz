31d2323182e51dd8a66960e47a98ff93291e3a53
Jin Hu, Baek <m@ake.kr>
2012-12-20 22:44:56 +0900

    레이아웃 - 표기변경 / 코믹뷰어 - 단축키 추가, 자바스크립트 유연성 강화(소스단축)
    이외에 라이센스 파일 삭제

--- plugin/album/common.js
+++ plugin/album/common.js
@@ -1,3 +1,5 @@
+var imageCount = nameArray.length-1;
+
 linkArray[-1] = '/?action=PluginResource&type=album&type=album_no_left.png';
 nameArray[-1] = 'no image';
 
@@ -37,18 +39,16 @@ function onClickPrev(){
 function Redraw(){
 	var prevIdx = currentImage - 1;
 	var nextIdx = currentImage + 1;
-	var zoomAttribute = '';
-	if( zoomRatio != 'real' ) zoomAttribute = " style='width:"+zoomRatio+"'";
-
+	var imgClass = '';
+	if (zoomRatio != 'real') { imgClass = ' '+zoomRatio; }
 	//cachedImage.src = linkArray[nextIdx];
 
 	var imagePane = document.getElementById('imagePane');
-	imagePane.innerHTML = '<a href="#'+nextIdx+'" onClick="onClickNext()"><img class="main" src="'+linkArray[currentImage]+'" '+zoomAttribute+' /></a>';
+	imagePane.innerHTML = '<a href="#'+nextIdx+'" onClick="onClickNext()"><img id="albumimg" class="main'+imgClass+'" src="'+linkArray[currentImage]+'" /></a>';
 
 	var filenamePane = document.getElementById('filenamePane');
 	filenamePane.innerHTML = nameArray[currentImage] + " &nbsp; (" + (currentImage) + "/" + Number(imageCount-1) + ")";
 
-	///////////////////////////
 	// 좌우 이동 버튼
 	if(currentImage > 0) { // 이전 페이지로 이동 가능
 		document.getElementById('button_left_on').style.display = 'block';
@@ -78,23 +78,6 @@ function onZoomChange(sel){
 }
 
 var listIsShown = false;
-/*
-function onToggleList(){
-	listIsShown = !listIsShown;
-
-	var fileList = document.getElementById('fileList');
-	var wrap = document.getElementById("wrap");
-	if(listIsShown){
-		RedrawList();
-		fileList.style.display = 'block';
-		wrap.style.margin = "0 0 0 220px";
-	}
-	else{
-		fileList.style.display = 'none';
-		wrap.style.margin = "0";
-	}
-}
-*/
 
 function RedrawList()
 {
@@ -107,4 +90,17 @@ function RedrawList()
 
 	var fileList = document.getElementById('fileList');
 	fileList.innerHTML = '<ul class="list-vertical">'+stringBuf+'</ul>';
+}
+
+//key control enable
+window.document.onkeydown = function(e) {
+	if(!e){ e = event; }
+	//before page
+	if(e.keyCode == 37 && currentImage>0) {
+		onClickPrev();
+		location.href="#"+Number(window.location.hash.substr(1));
+	}
+	if(e.keyCode == 39 && currentImage<imageCount-1) {
+		onClickNext();
+	}
 }
